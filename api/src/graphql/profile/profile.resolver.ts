c3817d5980fb0fb547d641717e9e9119d0be22b5
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-10-15 16:23:04 -0300

    feat: create profile resolvers.

--- api/src/graphql/profile/profile.resolver.ts
+++ api/src/graphql/profile/profile.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Profile } from './profile.type';
+import { createProfileDTO } from './dtos/createProfileDTO';
+import { ProfileService } from './profile.service';
 
 @Resolver()
-export class ProfileResolver {}
+export class ProfileResolver {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Mutation(() => Profile)
+  async createProfile(@Args('input') input: createProfileDTO) {
+    return this.profileService.createProfile(input);
+  }
+
+  @Query(() => Profile)
+  async getProfileById(@Args('profileId') profileId: string) {
+    return this.profileService.getProfileById(profileId);
+  }
+
+  @Query(() => Profile)
+  async getProfileByEmail(@Args('profileEmail') profileEmail: string) {
+    return this.profileService.getProfileByEmail(profileEmail);
+  }
+}
