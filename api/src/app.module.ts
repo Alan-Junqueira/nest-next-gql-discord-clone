13dc6f4d7b6cdafbc28df778416be25a4b6818a1
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-10-15 10:34:21 -0300

    feat: Setup graphql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { GraphqlResolver } from './graphql/graphql.resolver';
+import { GraphqlService } from './graphql/graphql.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
+    GraphQLModule.forRootAsync({
+      imports: [],
+      inject: [],
+      driver: ApolloDriver,
+      useFactory: async () => {
+        return {
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+          sortSchema: true,
+          subscriptions: {},
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GraphqlService, GraphqlResolver],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+  });
+  app.use(
+    graphqlUploadExpress({
+      maxFieldSize: 10000000,
+      maxFiles: 1,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
