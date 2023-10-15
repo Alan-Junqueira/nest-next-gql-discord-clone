914eb773ac2e5217be05517e92dad7526214efd2
alan-junqueira <contato.alanjunqueira@gmail.com>
2023-10-15 16:14:23 -0300

    feat: create profile service methods.

---
+++ api/src/graphql/profile/profile.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { createProfileDTO } from './dtos/createProfileDTO';
+
+@Injectable()
+export class ProfileService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createProfile(data: createProfileDTO) {
+    const existingProfile = await this.prisma.profile.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (existingProfile) {
+      return existingProfile;
+    }
+
+    const newProfile = await this.prisma.profile.create({
+      data,
+    });
+
+    return newProfile;
+  }
+
+  async getProfileById(id: string) {
+    const profile = await this.prisma.profile.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        servers: {
+          include: {
+            channels: true,
+          },
+        },
+      },
+    });
+
+    return profile;
+  }
+
+  async getProfileByEmail(email: string) {
+    const profile = await this.prisma.profile.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        servers: {
+          include: {
+            channels: true,
+          },
+        },
+      },
+    });
+
+    return profile;
+  }
+}
